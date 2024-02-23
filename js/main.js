f6228916e9b7392bb77fbc8532c46531432f81eb
Boris Hasikov <hasikov@laapl.ru>
2024-02-24 02:53:55 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   Telegram.WebApp.ready();
   Telegram.WebApp.expand();
-
-  let clicked = false;
-
-  document.querySelector("canvas").addEventListener("click", () => {
-    if (clicked) {
-      Telegram.WebApp.HapticFeedback.notificationOccurred("error");
-    } else {
-      Telegram.WebApp.HapticFeedback.notificationOccurred("success");
-    }
-
-    clicked = !clicked;
-  });
 });
